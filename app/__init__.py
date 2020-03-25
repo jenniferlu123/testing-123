ff376df9dfbf9a3316a8d097c5bf11b7faf4c93e
jenniferlu123 <59629822+jenniferlu123@users.noreply.github.com>
2020-03-25 17:36:10 -0400

    with travis file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 # nothing to see here
 
-# helps other commands recognize that there are files within 
-# directory which we want to import from
+# helps other commands to recognize that there are files within this directory which we want to import from
 
 # the directory wehre this __init__.py file exits, treat as a module
--- conftest.py
+++ conftest.py
@@ -1,1 +1,4 @@
+# helps with testing
+
+
 # put this in the root directory of your directory
